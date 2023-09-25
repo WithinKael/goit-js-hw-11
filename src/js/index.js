8b5ce0fe9c0d35f7d6f7f2099bfad19f2ae97ac7
WithinKael <138517664+WithinKael@users.noreply.github.com>
2023-09-25 15:00:04 +0200

    update simplelightbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import { PixabayAPI } from './photosapi';
 import Notiflix from 'notiflix';
 import { addClass, removeClass } from './hidebtn';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('.search-form');
 const gallaryEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
+let gallery = null;
 
 const pixabayAPI = new PixabayAPI();
 
@@ -37,6 +40,13 @@ async function onFormElSubmit(event) {
     gallaryEl.innerHTML = renderPhotos(response.data.hits);
     Notiflix.Notify.info(`Hooray! We found ${response.data.totalHits} images`);
     removeClass();
+
+    setTimeout(() => {
+      gallery = new SimpleLightbox('.gallery a', {
+        docClose: false,
+        captionDelay: 250,
+      });
+    }, 10);
   } catch (error) {
     console.log(error);
   }
@@ -51,6 +61,7 @@ async function onLoadMoreBtn(event) {
     const response = await pixabayAPI.getImages();
     gallaryEl.insertAdjacentHTML('beforeend', renderPhotos(response.data.hits));
     removeClass();
+    gallery.refresh();
 
     if (Math.ceil(response.data.totalHits / 40) < pixabayAPI.page) {
       addClass();
@@ -76,23 +87,25 @@ function renderPhotos(data) {
   const markup = data
     .map(element => {
       return `
-      <div class="photo-card">
-        <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" class="gallery-img"/>
-        <div class="info" class="info-wrapper">
-          <p class="info-item">
-            <b>Likes: ${element.likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views: ${element.views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments: ${element.comments}</b>
-          </p>
-          <p class="info-item">
-             <b>Downloads: ${element.downloads}</b>
-          </p>
-        </div>
-      </div>  
+      <a href="${element.largeImageURL}">
+        <div class="photo-card">
+          <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" class="gallery-img"/>
+          <div class="info" class="info-wrapper">
+            <p class="info-item">
+              <b>Likes: ${element.likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${element.views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${element.comments}</b>
+            </p>
+            <p class="info-item">
+               <b>Downloads: ${element.downloads}</b>
+            </p>
+          </div>
+        </div>  
+      </a>
     `;
     })
     .join('\n');
